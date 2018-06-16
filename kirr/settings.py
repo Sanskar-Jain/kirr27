21f9086b1d374863667578b49b672737ed55aba9
Sanskar Jain <sanskar27jain@gmail.com>
2018-06-17 01:09:51 +0530

    Fixed error templates

--- kirr/settings.py
+++ kirr/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'kirr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),os.path.join(BASE_DIR, 'templates','shortener')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
